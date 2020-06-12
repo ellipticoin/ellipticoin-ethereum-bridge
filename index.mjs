f34484813a3e87933bf03c715bb2992b0ca583b3
Mason Fischer <mason@kissr.co>
2020-06-12 08:16:30 -0400

    Use IPC

--- index.mjs
+++ index.mjs
@@ -5,12 +5,14 @@ import cbor from "borc";
 import ecClient from "ec-client";
 import ethers from "ethers";
 import fs from "fs";
+import net from "net";
 import os from "os";
 import _ from "lodash";
 import websocket from "websocket";
 import uniswapSDK from "@uniswap/sdk";
 import Web3 from "web3";
 const {
+  WETH,
   ChainId,
   Token,
   TokenAmount,
@@ -20,9 +22,11 @@ const {
   Trade,
   Percent,
 } = uniswapSDK;
-dotenv.config();
 const WebSocketClient = websocket.w3cwebsocket;
-const INFURA = new ethers.providers.InfuraProvider(
+dotenv.config();
+let ipcPath = process.env["HOME"] + "/.local/share/openethereum/jsonrpc.ipc";
+const IPC_PROVIDER = new ethers.providers.IpcProvider(ipcPath);
+const INFURA_PROVIDER = new ethers.providers.InfuraProvider(
   process.env.INFURA_NETWORK,
   process.env.INFURA_API_KEY
 );
@@ -37,73 +41,77 @@ const BRIDGE_CONTRACT_ADDRESS = Buffer.concat([
 
 const { Ellipticoin, transactionHash } = ecClient;
 const ECClient = ecClient.Client;
-let ellipticoinBalance;
-
 const CLIENT = ECClient.fromConfig(`${os.homedir()}/.ec-wallet/config.yaml`);
-// CLIENT.bootnodes = ["http://localhost:4461"];
 Ellipticoin.client = CLIENT;
 const ECCB_ABI = JSON.parse(
-  fs.readFileSync(
-    "ellipticoin_bridge/artifacts/ECCBToken.json",
-    "utf8"
-  )
+  fs.readFileSync("ellipticoin_bridge/artifacts/ECCBToken.json", "utf8")
 ).abi;
-const WALLET = new ethers.Wallet(process.env.ETHEREUM_PRIVATE_KEY, INFURA);
-const ECCB_ADDRESS = "0x89108B7Bf410Ce52e41eFd41e565ca180Df1EC53";
-
-const ECCB = new ethers.Contract(ECCB_ADDRESS, ECCB_ABI, WALLET);
+const WETH_TOKEN = new Token(ChainId.MAINNET, WETH[ChainId.MAINNET].address, 4);
+const ECCB_TOKEN = new Token(
+  ChainId.MAINNET,
+  "0x8b4da1ccC931Eb26e70E86d9706517Ce2DBF0Ad1",
+  4
+);
+const WALLET = new ethers.Wallet(
+  process.env.ETHEREUM_PRIVATE_KEY,
+  INFURA_PROVIDER
+);
+console.log(WALLET.address);
+const ECCB = new ethers.Contract(ECCB_TOKEN.address, ECCB_ABI, WALLET);
 const ROUTER_ABI = JSON.parse(
-  fs.readFileSync(
-    "ellipticoin_bridge/artifacts/UniswapV2Router01.json",
-    "utf8"
-  )
+  fs.readFileSync("ellipticoin_bridge/artifacts/UniswapV2Router01.json", "utf8")
 ).abi;
-const ROUTER_ADDRESS = "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a";
+const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 const ROUTER = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, WALLET);
 
 const {
   utils: { parseEther },
 } = ethers;
 
-const WETH_TOKEN = new Token(
-  ChainId.KOVAN,
-  "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
-  18,
-);
-const ECCB_TOKEN = new Token(
-  ChainId.KOVAN,
-  ECCB_ADDRESS,
-  4,
-);
-const SLIPPAGE = new Percent(5, 1000);
-let web3 = new Web3("wss://kovan.infura.io/ws/v3/28d900c929bf4df88e0a4adc9f790e22");
-const instance = new web3.eth.Contract(ECCB_ABI, ECCB_ADDRESS);
-instance.events.SwapAndBurn({
+const SLIPPAGE = new Percent(10, 1000);
+var transactionHashes = [];
+var web3 = new Web3(ipcPath, net);
+const instance = new web3.eth.Contract(ECCB_ABI, ECCB.address);
+instance.events
+  .SwapAndBurn({
     filter: {},
-    fromBlock: "latest"
-})
-.on('data', async function(event){
-    let address = Buffer.from(event.returnValues.ellipticoin_address.substring(2), "hex")
-    console.log(event.returnValues.amount)
-    let amount = parseInt(event.returnValues.amount)
-    console.log(await CLIENT.post({
+    fromBlock: "latest",
+  })
+  .on("data", async function (event) {
+    let address = Buffer.from(
+      event.returnValues.ellipticoin_address.substring(2),
+      "hex"
+    );
+    let amount = parseInt(event.returnValues.amount);
+    console.log(
+      `Processing buy: https://etherscan.io/tx/${event.transactionHash}`
+    );
+    if (event.transactionHash in transactionHashes) {
+      console.log(
+        `Already processed https://etherscan.io/tx/${event.transactionHash}. Skipping`
+      );
+      return;
+    }
+    transactionHashes.push(transactionHashes);
+    let transfer = await CLIENT.post({
       contract_address: Buffer.concat([
         ELLIPTICOIN_ADDRESS,
         Buffer.from("EthereumBridge", "utf8"),
       ]),
       function: "transfer",
-      arguments: [
-        Array.from(address),
-        Math.floor(amount),
-      ],
-    }));
-    
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', () => console.error('error'))
-.on('close', () => console.error('closed'));
+      arguments: [Array.from(address), Math.floor(amount)],
+    });
+    console.log(
+      `Processed buy: https://block-explorer.ellipticoin.org/transactions/${base64url(
+        transactionHash(transfer)
+      )}`
+    );
+  })
+  .on("changed", function (event) {
+    console.log("changed")
+  })
+  .on("error", () => console.error("error"))
+  .on("close", () => console.error("closed"));
 
 async function mintAndSwap(amount, address, ellipticoinTransactionHash) {
   let pair = await Pair.fetchData(WETH_TOKEN, ECCB_TOKEN);
@@ -113,24 +121,19 @@ async function mintAndSwap(amount, address, ellipticoinTransactionHash) {
     new TokenAmount(ECCB_TOKEN, amount),
     TradeType.EXACT_INPUT
   );
-
+  console.log(`Processing sell: https://block-explorer.ellipticoin.org/transactions/${base64url(ellipticoinTransactionHash)}`)
   let { hash } = await ECCB.mintAndSwap(
-    trade.maximumAmountIn(SLIPPAGE).raw.toString(),
-    trade.minimumAmountOut(SLIPPAGE).raw.toString(),
-    trade.route.path.map((t) => t.address),
-    address,
-    Math.ceil(Date.now() / 1000) + 60 * 20,
-    ellipticoinTransactionHash,
-    {
-      gasLimit: 5000000,
-    }
-  );
-  console.log(
-    `Minted and swapped: ${amount} EC https://kovan.etherscan.io/tx/${hash}`
-  );
+     trade.maximumAmountIn(SLIPPAGE).raw.toString(),
+     trade.minimumAmountOut(SLIPPAGE).raw.toString(),
+     trade.route.path.map((t) => t.address),
+     address,
+     Math.ceil(Date.now() / 1000) + 60 * 20,
+     ellipticoinTransactionHash,
+   );
+  console.log(`Processed sell: https://etherscan.io/tx/${hash}`)
 }
 
-var client = new WebSocketClient("ws://localhost:4462/websocket");
+var client = new WebSocketClient("wss://davenport.ellipticoin.org/websocket");
 setInterval(() => client.send(new ArrayBuffer(0)), 30000);
 client.onmessage = ({ data }) => processBlock(decodeBytes(data));
 const decodeBytes = (bytes) => cbor.decode(Buffer.from(bytes));
